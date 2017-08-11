b9911a0fb33e92d1d59c3312e2aa3b74a44563cf
Andy Tuttle <andytuttle12@gmail.com>
2017-08-10 22:54:37 -0400

    Added controlled text components to app.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBzjBRO08mbVKZn6rdULVyqx-R2EnIKYTk",
+            authDomain: "react-native-auth-8ccba.firebaseapp.com",
+            databaseURL: "https://react-native-auth-8ccba.firebaseio.com",
+            projectId: "react-native-auth-8ccba",
+            storageBucket: "react-native-auth-8ccba.appspot.com",
+            messagingSenderId: "190876258649"
+        });
+    }
+
     render() {
         return (
             <View>
-                <Text>App</Text>
+                <Header headerText="Authentication" />
+                <LoginForm />
             </View>
         );
     }
