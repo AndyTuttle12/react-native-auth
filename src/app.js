646aa66d4b93ce1e1ffa270190cab3a1de9c28ae
Andy Tuttle <andytuttle12@gmail.com>
2017-08-11 23:20:14 -0400

    Added login form, passed login methods, and rendered conditional components with spinner, and handled promises.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
     componentWillMount() {
         firebase.initializeApp({
             apiKey: "AIzaSyBzjBRO08mbVKZn6rdULVyqx-R2EnIKYTk",
@@ -14,13 +15,36 @@ class App extends Component {
             storageBucket: "react-native-auth-8ccba.appspot.com",
             messagingSenderId: "190876258649"
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            }else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Button>
+                        Log Out
+                    </Button>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner />;
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
